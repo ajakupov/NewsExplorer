9bdcf9b138c5b7482635b308050af11aeb047fde
ajakupov <jakupov.iitu@gmail.com>
2022-08-09 15:49:14 +0200

    style: remove .idea files

---
+++ beans/news_answer.py
@@ -0,0 +1,6 @@
+class NewsAnswer:
+    def __init__(self, json_response):
+        self.id = None
+        self.read_link = None
+        self.related_topics = None
+        self.total_estimated_matches = None
---
+++ beans/news_article.py
@@ -0,0 +1,12 @@
+class NewsArticle:
+    def __init__(self, json_response):
+        self.about = None
+        self.category = None
+        self.clustered_articles = None
+        self.contractual_rules = None
+        self.date_published = None
+        self.description = None
+        self.id = None
+        self.name = None
+        self.provider = None
+        self.url = None
